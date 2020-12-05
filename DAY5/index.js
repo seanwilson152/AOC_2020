313f676a9ce4cf808cdcb9b785869645d5c71eb1
Sean Wilson <Sean.Wilson@analog.com>
2020-12-05 21:36:13 +0000

    optimized DAY5 solution

--- DAY5/index.js
+++ DAY5/index.js
@@ -6,25 +6,18 @@ Note: These solutions should be executed via the AoC20.js launcher
 
 getSeatNumber = seat => {
 	// just convert the F,B,R,L into binary 1/0 and parse as int
-	var row = seat.substring(0,7).split("F").join("0").split("B").join("1")
-	row = parseInt(row,2)
-
-	var column = seat.substring(7,10).split("R").join("1").split("L").join("0")
-	column = parseInt(column, 2)
-
+	const binary = seat.split(/F|L/).join("0").split(/B|R/).join("1")
+	const row = parseInt(binary.substring(0,7),2)
+	const column = parseInt(binary.substring(7,10),2)
 	return (row * 8 ) + column
 }  
 
 part1 = inputData => {
-	return inputData.reduce((acc,seat) =>{
-		const seatNumber = getSeatNumber(seat)
-		return  seatNumber > acc ? seatNumber : acc
-	}, 0)
+	return Math.max(...inputData.map(getSeatNumber))
 }
 
 part2 = inputData => {
-	var allSeats = inputData.map(seat=>{return getSeatNumber(seat)})
-	allSeats = allSeats.sort((a, b) => {return a-b})
+	var allSeats = inputData.map(getSeatNumber).sort((a, b) => {return a-b})
 
 	return allSeats.reduce( (acc, seat, index) => {
 		return seat - 1 == allSeats[index-1] ? acc : seat -1
